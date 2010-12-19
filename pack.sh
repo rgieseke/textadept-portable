6e7fe37ad596510e5e4243d088bda5a865b71488
rgieseke <robert.gieseke@gmail.com>
2010-12-19 18:07:50 +0100

    Updated.

--- pack.sh
+++ pack.sh
@@ -1,6 +1,5 @@
 rm -rf TextadeptPortable/Data/.textadept
 cp -r dot_textadept TextadeptPortable/Data/.textadept
-rm TextadeptPortable/App/GTK/gtk2_runtime_uninst.exe
 rm TextadeptPortable.zip
 pushd TextadeptPortable
 zip -r ../TextadeptPortable.zip .
